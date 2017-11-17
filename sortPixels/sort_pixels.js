54a85c675158bd09ba37b8f46146043ea9cf1973
mlafore3 <mlafore3@uwo.ca>
2017-11-17 08:43:09 -0500

    pixel rearranging

--- sortPixels/sort_pixels.js
+++ sortPixels/sort_pixels.js
@@ -4,18 +4,27 @@
 // 	console.log(data);
 // }
 
-var photo = new Image();
-photo.src = 'light.png';
+// $.ajax({
+// 	url: 'https://github.com/mlafore3/generative_design/blob/master/sortPixels/light.png',
+// 	type: 'GET',
+// 	success: function (response){
+// 		var photo = new Image();
+// 		photo.src = response;
+// 		console.log(response);
+// 	}
+// })
 
 document.addEventListener("DOMContentLoaded", function() {
 	var can = document.getElementById('image_canvas');
-	photo.onload = function () {
-		can.width = photo.width;
-		can.height = photo.height;
-		can.getContext("2d").drawImage(photo, 0, 0);
-		var imageData = can.getImageData(0, 0, 300, 300);
-		var data = imageData.data;
-		console.log(data);
+	var photoSrc = document.getElementById('image');
+	var photo = new Image();
+	photo.src = photoSrc.src;
+	can.width = photo.width;
+	can.height = photo.height;
+	var ctx = can.getContext("2d");
+	ctx.drawImage(photo, 0, 0);
+	var imageData = ctx.getImageData(0, 0, can.width, can.height);
+	var data = imageData.data;
+	console.log(data);
 		//manipulatePixels(can);
-	}
 });
